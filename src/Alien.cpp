506e5834fb7603c29cd4994cbc87a52e8db3e5b4
Eduardo Marciano <89046882+EduardoMarciano@users.noreply.github.com>
2023-10-19 00:43:21 -0300

    refactor: moviment

--- src/Alien.cpp
+++ src/Alien.cpp
@@ -23,28 +23,36 @@ void Alien::Update(float dt) {
     else if (InputManager::GetInstance().MousePress(SDL_BUTTON_RIGHT)){
         taskQueue.emplace(Action(MOVE, InputManager::GetInstance().GetMouseX(), InputManager::GetInstance().GetMouseY()));
     }
-
+    speed.x = 150;
+    speed.y = 150;
     if (!taskQueue.empty()) {
-        float step = dt * 150;
+        Action& item = taskQueue.front();
 
-    Vec2 targetPosition(taskQueue.front().pos.x, taskQueue.front().pos.y);
-    Vec2 currentPosition(associated.box.x + associated.box.w / 2, associated.box.y + associated.box.h / 2);
+        if (item.type == SHOOT) {
+            taskQueue.pop();
 
-    Vec2 direction = targetPosition - currentPosition;
-    float distance = direction.Magnitude();
+        } else if (item.type == MOVE) {
+            Vec2 targetPosition(item.pos.x, item.pos.y);
+            Vec2 direction = (targetPosition - Vec2(associated.box.x, associated.box.y));
+            
+            direction.Normalize();
 
-    if (distance > step) {
-        direction.Normalize();
-        Vec2 velocity = direction * step;
-        associated.box.x += velocity.x;
-        associated.box.y += velocity.y;
-    } else {
-        associated.box.x = targetPosition.x - associated.box.w / 2;
-        associated.box.y = targetPosition.y - associated.box.h / 2;
-        taskQueue.pop();
+            float distance = (targetPosition - Vec2(associated.box.x, associated.box.y)).Magnitude();
+            float moveDistance = speed.Magnitude() * dt;
+
+            if (distance <= moveDistance) {
+
+                associated.box.x = targetPosition.x - associated.box.w / 2;
+                associated.box.y = targetPosition.y - associated.box.h / 2;
+                taskQueue.pop();
+            } else {
+
+                Vec2 velocity = direction * (speed.Magnitude() * dt);
+                associated.box.x += velocity.x;
+                associated.box.y += velocity.y;
+            }
+        }
     }
-}
-        
     if (hp <= 0) {
         associated.RequestDelete();
     }
