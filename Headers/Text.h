4427ed77393c24c3abad5505f597616537aafb60
Eduardo Marciano <89046882+EduardoMarciano@users.noreply.github.com>
2023-11-13 12:32:48 -0300

    feat: text

---
+++ Headers/Text.h
@@ -0,0 +1,17 @@
+#ifndef Text_h
+#define Text_h
+
+#include "Component.h"
+#include "GameObject.h"
+enum TextStyle = ();
+
+class Text : public Component{
+    public:
+        Text
+    private:
+
+};
+
+
+
+#endif
